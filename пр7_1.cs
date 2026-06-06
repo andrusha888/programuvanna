d8b0755582c93a2771154e518d80f7423b9b3af5
Andrusha <151789946+andrusha888@users.noreply.github.com>
2026-06-06 18:01:57 +0300

    Add files via upload

---
+++ пр7_1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+
+class StackNumbers
+{
+    private int[] data;
+    private int top;
+
+    public StackNumbers(int size)
+    {
+        data = new int[size];
+        top = -1;
+    }
+
+    public bool IsEmpty()
+    {
+        return top == -1;
+    }
+
+    public bool IsFull()
+    {
+        return top == data.Length - 1;
+    }
+
+    public void Push(int value)
+    {
+        if (IsFull())
+        {
+            Console.WriteLine("Стек заповнений. Неможливо додати елемент.");
+            return;
+        }
+
+        top++;
+        data[top] = value;
+    }
+
+    public int Pop()
+    {
+        if (IsEmpty())
+        {
+            Console.WriteLine("Стек порожній. Неможливо вилучити елемент.");
+            return 0;
+        }
+
+        int value = data[top];
+        top--;
+        return value;
+    }
+
+    public void PrintStack()
+    {
+        if (IsEmpty())
+        {
+            Console.WriteLine("Стек порожній.");
+            return;
+        }
+
+        Console.WriteLine("Елементи стеку:");
+
+        for (int i = top; i >= 0; i--)
+        {
+            Console.WriteLine(data[i]);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.Title = "Практична робота №7, Таран Андрій";
+
+        Console.WriteLine("Виконав: Таран Андрій");
+        Console.WriteLine("Практична робота №7 Навчальне завдання 1");
+        Console.WriteLine();
+
+        StackNumbers stack = new StackNumbers(5);
+
+        stack.Push(10);
+        stack.Push(25);
+        stack.Push(40);
+
+        Console.WriteLine("Після додавання елементів:");
+        stack.PrintStack();
+
+        Console.WriteLine();
+
+        int removed = stack.Pop();
+
+        Console.WriteLine("Вилучений елемент зі стеку: " + removed);
+        Console.WriteLine();
+
+        Console.WriteLine("Стек після вилучення:");
+        stack.PrintStack();
+    }
+}
---
+++ пр7_2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+class Program
+{
+    static void PrintStack(Stack<string> stack)
+    {
+        if (stack.Count == 0)
+        {
+            Console.WriteLine("Стек порожній.");
+            return;
+        }
+
+        Console.WriteLine("Елементи стеку:");
+
+        foreach (string word in stack)
+        {
+            Console.WriteLine(word);
+        }
+    }
+
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.Title = "Практична робота №7, Таран Андрій";
+
+        Console.WriteLine("Виконав: Таран Андрій");
+        Console.WriteLine("Практична робота №7 Навчальне завдання 2.");
+        Console.WriteLine();
+
+        Stack<string> words = new Stack<string>();
+
+        words.Push("алгоритм");
+        words.Push("масив");
+        words.Push("стек");
+        words.Push("програма");
+
+        Console.WriteLine("Після додавання слів:");
+        PrintStack(words);
+
+        Console.WriteLine();
+
+        if (words.Count > 0)
+        {
+            string removedWord = words.Pop();
+
+            Console.WriteLine("Вилучений елемент зі стеку: " + removedWord);
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("Стек після вилучення:");
+        PrintStack(words);
+    }
+}
